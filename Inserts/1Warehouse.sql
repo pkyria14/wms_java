9ae42aaa24c8f9acc6daf391d78ab12cf0d0d0f9
mpitsi04 <36662287+mpitsi04@users.noreply.github.com>
2019-11-22 22:56:12 +0200

    Add files via upload

---
+++ Inserts/1Warehouse.sql
@@ -0,0 +1,1 @@
+INSERT INTO Transactions(WarehouseID,Location,Capacity) VALUES (1,'Dali',240);
---
+++ Inserts/1logFile.sql
@@ -0,0 +1,1 @@
+INSERT INTO LogFile(LogFileNo,Date,Report) VALUES (1,'12/19/2018','Updated employee to admin');
